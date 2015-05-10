2f27605030622b76f6155629f800d9a5f1bed32d
rassoh <rasso@basics09.de>
2015-05-10 23:28:41 +0200

    Add Message and Files

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>@hirasso</title>
+  <link rel="stylesheet" type="text/css" href="assets/style.css">
+</head>
+<body>
+  <div class="wrap">
+    You have found an empty space in the internet. <br>
+    It belongs to <a href="https://github.com/hirasso/">me</a>.
+  </div>
+  <script type="text/javascript" src="assets/app.js"></script>
+</body>
+</html>
